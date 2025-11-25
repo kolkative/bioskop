bd587d1294a5243b96361b930c28c0f17251da02
Kolkative <kolkative@gmail.com>
2025-11-25 12:10:22 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   srcDir: './web/src',
-  outDir: './web/templates',
+  outDir: './web/dist',
   publicDir: './blahblah',
   build: {
     format: 'file',
